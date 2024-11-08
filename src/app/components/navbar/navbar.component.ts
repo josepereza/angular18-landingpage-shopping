2a88608700549e5e0f9f48f58b5fb3acaba0b4d6
josepereza <josepereza66@gmail.com>
2024-11-08 11:35:33 +0100

    first commit3

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,15 +3,16 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { CartService } from '../../services/cart.service';
 import { RouterLink } from '@angular/router';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,RouterLink],
+  imports: [MatToolbarModule,MatIconModule,RouterLink,MatBadgeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 cartService= inject(CartService)
-
+numero=5
 }
